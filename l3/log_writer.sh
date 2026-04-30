d3bee52dfad19e0e43e14b010535dd9ee58e804a
andre <andre@gmail.com>
2026-04-30 13:22:19 +0300

    feat: add log_writer with sys calls

---
+++ l3/log_writer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LOG_FILE="${SCRIPT_DIR}/logs/basic.log"
+
+mkdir -p "${SCRIPT_DIR}/logs"
+
+> $LOG_FILE
+
+trap 'echo "[ $(date +"%T") ] Process was completed correctly" >> $LOG_FILE && exit 0' SIGTERM 
+trap '$((NUMBER=0)) && echo "[ $(date +"%T") ] Process was restarted" > $LOG_FILE' SIGHUP
+trap 'echo "[ $(date +"%T") ] This process has been touched" >> $LOG_FILE' SIGUSR1
+
+NUMBER=0
+
+while true
+do
+echo "[ $(date +'%T') ] Log ${NUMBER}" >> $LOG_FILE
+sleep 1
+((NUMBER+=1))
+done
